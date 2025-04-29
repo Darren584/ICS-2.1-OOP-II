f87159005269452dd279c9ccba4b019f7eb4f5d9
Darren584 <darren.alweyi@strathmore.edu>
2025-04-29 14:06:03 +0300

    Create Challenge2.java

---
+++ Challenge2.java
@@ -0,0 +1,6 @@
+public class Challenge2 {
+    public static void main(String[] args) {
+        String favoriteColor = "Blue";  // Declaring and assigning a variable
+        System.out.println("My favorite color is: " + favoriteColor);
+    }
+}
