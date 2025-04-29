fbace267ea6b59fd74e3d75fe155d43fde8d8be4
Darren584 <darren.alweyi@strathmore.edu>
2025-04-29 14:05:21 +0300

    Create Challenge1.java

---
+++ Challenge1.java
@@ -0,0 +1,10 @@
+public class Challenge1 {
+    public static void main(String[] args) {
+        displayWelcome();  // Calling a function
+    }
+
+    // Function that prints a welcome message
+    public static void displayWelcome() {
+        System.out.println("Welcome to the Guessing Game!");
+    }
+}
