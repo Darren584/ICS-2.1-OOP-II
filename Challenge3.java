226a420e58a7dce05b195dd10a7d4befb3b0b396
Darren584 <darren.alweyi@strathmore.edu>
2025-04-29 14:06:26 +0300

    Create Challenge3.java

---
+++ Challenge3.java
@@ -0,0 +1,13 @@
+public class Challenge3 {
+    public static void main(String[] args) {
+        int score = 75;
+
+        if (score >= 80) {
+            System.out.println("You passed with distinction!");
+        } else if (score >= 50) {
+            System.out.println("You passed!");
+        } else {
+            System.out.println("You failed. Try again!");
+        }
+    }
+}
