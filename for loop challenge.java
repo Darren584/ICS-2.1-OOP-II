9a657b2deb1b5dd215be1b9f43cb129d4c00be8d
Darren584 <darren.alweyi@strathmore.edu>
2025-07-13 22:27:55 +0300

    for loop challenge.java

---
+++ for loop challenge.java
@@ -0,0 +1,11 @@
+public class AssignmentSubmission {
+    public static void main(String[] args) {
+        // Simulate submitting 5 assignments
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Submitting assignment #" + i);
+        }
+
+        System.out.println("All assignments submitted!");
+    }
+}
